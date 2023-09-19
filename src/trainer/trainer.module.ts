ad3c89525bc5810318d962b556db0107e8c1e517
Taemin <kwaktaemin98@gmail.com>
2023-09-19 14:33:39 +0900

    추가: imports에 typeOrmModule 추가

--- src/trainer/trainer.module.ts
+++ src/trainer/trainer.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TrainerService } from './trainer.service';
 import { TrainerResolver } from './trainer.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  providers: [TrainerResolver, TrainerService]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [TrainerResolver, TrainerService],
 })
 export class TrainerModule {}
