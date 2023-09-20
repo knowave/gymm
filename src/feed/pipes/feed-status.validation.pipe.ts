15a928a88387e7a61cc7569a6e641a70332c90f8
Taemin <kwaktaemin98@gmail.com>
2023-09-20 10:12:46 +0900

    생성: Feed 수정 시 사용 할 Feed Status Validation Pipe 생성

---
+++ src/feed/pipes/feed-status.validation.pipe.ts
@@ -0,0 +1,22 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { FeedStatus } from '../entities/feed.entity';
+
+export class FeedStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [FeedStatus.PRIVATE, FeedStatus.PUBLIC];
+
+  transform(value: any): any {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException('올바르지 않은 상태값 입니다.');
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+
+    return index !== -1;
+  }
+}
