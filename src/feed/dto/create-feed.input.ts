381467bcdd9a6b0ed3f90a84ab07291333fb7c93
Taemin <kwaktaemin98@gmail.com>
2023-09-20 09:55:14 +0900

    생성: Feed 생성 관련 DTO 생성

--- src/feed/dto/create-feed.input.ts
+++ src/feed/dto/create-feed.input.ts
@@ -1,7 +1,9 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, ObjectType, PartialType } from '@nestjs/graphql';
+import { Feed } from '../entities/feed.entity';
+import { BaseOutput } from 'src/common/base-output.dto';
 
 @InputType()
-export class CreateFeedInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
-}
+export class CreateFeedInput extends PartialType(Feed) {}
+
+@ObjectType()
+export class CreateFeedOutput extends BaseOutput {}
