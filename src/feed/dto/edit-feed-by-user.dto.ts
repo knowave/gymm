0410de11bbba66f7fce91b7513be7cd395ed010a
knowave <kwaktaemin98@gmail.com>
2023-09-29 12:04:37 +0900

    추가: Feed 수정을 위한 EditFeedByUser DTO 생성

---
+++ src/feed/dto/edit-feed-by-user.dto.ts
@@ -0,0 +1,21 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Feed, FeedStatus } from '../entities/feed.entity';
+import { BaseOutput } from 'src/common/dto/base-output.dto';
+
+@InputType()
+export class EditFeedByUserInput {
+  @Field(() => Number)
+  feedId: number;
+
+  @Field(() => String, { nullable: true })
+  title?: string;
+
+  @Field(() => String, { nullable: true })
+  description?: string;
+
+  @Field(() => FeedStatus, { nullable: true })
+  status?: FeedStatus;
+}
+
+@ObjectType()
+export class EditFeedByUserOutput extends BaseOutput {}
