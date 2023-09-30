0caf0140d0c6c3e25cfb557d135b2240f0e81b8e
knowave <kwaktaemin98@gmail.com>
2023-09-30 19:00:49 +0900

    생성 및 삭제: 예시용 DTO 삭제 및 트레이너 생성용 DTO 생성

---
+++ src/trainer/dto/create-trainer-by-user.input.ts
@@ -0,0 +1,15 @@
+import { InputType, Int, Field, ObjectType } from '@nestjs/graphql';
+import { BaseOutput } from 'src/common/dto/base-output.dto';
+import { UserRole } from 'src/user/enums/user-role.enum';
+
+@InputType()
+export class CreateTrainerByUserInput {
+  @Field(() => Number)
+  userId: number;
+
+  @Field(() => UserRole)
+  role: UserRole;
+}
+
+@ObjectType()
+export class CreateTrainerByUserOutput extends BaseOutput {}
