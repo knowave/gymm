b7f4391957d4ca6d489baf9dd2c6d05321c4cd34
knowave <kwaktaemin98@gmail.com>
2023-09-30 19:30:16 +0900

    추가: Gym 생성을 위한 createGymByTrainer 리졸버 추가

--- src/gym/gym.resolver.ts
+++ src/gym/gym.resolver.ts
@@ -1,7 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { GymService } from './gym.service';
+import {
+  CreateGymByTrainerInput,
+  CreateGymByTrainerOutput,
+} from './dto/create-gym-by-trainer.dto';
+import { User } from 'src/user/entities/user.entity';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { Role } from 'src/auth/decorators/user-role.decorator';
 
 @Resolver()
 export class GymResolver {
   constructor(private readonly gymService: GymService) {}
+
+  @Mutation(() => CreateGymByTrainerOutput)
+  @Role(['TRAINER'])
+  async createGymByTrainer(
+    @Args('input') createTrainerByUserInput: CreateGymByTrainerInput,
+    @CurrentUser() user: User,
+  ): Promise<CreateGymByTrainerOutput> {
+    return this.gymService.createGymByTrainer(createTrainerByUserInput, user);
+  }
 }
