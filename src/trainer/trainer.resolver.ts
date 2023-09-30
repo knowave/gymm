25a3cd596cbcc2a7840cd906d6f2320901bd33b4
knowave <kwaktaemin98@gmail.com>
2023-09-30 19:01:37 +0900

    추가: 트레이너 생성을 위한 createTrainerByUser 리졸버 추가

--- src/trainer/trainer.resolver.ts
+++ src/trainer/trainer.resolver.ts
@@ -1,7 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { TrainerService } from './trainer.service';
+import {
+  CreateTrainerByUserInput,
+  CreateTrainerByUserOutput,
+} from './dto/create-trainer-by-user.input';
 
 @Resolver()
 export class TrainerResolver {
   constructor(private readonly trainerService: TrainerService) {}
+
+  @Mutation(() => CreateTrainerByUserOutput)
+  async createTrainerByUser(
+    @Args('input') createTrainerByUserInput: CreateTrainerByUserInput,
+  ): Promise<CreateTrainerByUserOutput> {
+    return this.trainerService.createTrainerByUser(createTrainerByUserInput);
+  }
 }
