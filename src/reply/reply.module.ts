8446fe4a829ad93bd5d3e922b591b1c6f082206b
Taemin <kwaktaemin98@gmail.com>
2023-10-05 16:35:03 +0900

    Reply init

--- src/reply/reply.module.ts
+++ src/reply/reply.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReplyService } from './reply.service';
 import { ReplyResolver } from './reply.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Feed } from 'src/feed/entities/feed.entity';
+import { Gym } from 'src/gym/entities/gym.entity';
+import { Reply } from './entities/reply.entity';
 
 @Module({
-  providers: [ReplyResolver, ReplyService]
+  imports: [TypeOrmModule.forFeature([User, Feed, Gym, Reply])],
+  providers: [ReplyResolver, ReplyService],
 })
 export class ReplyModule {}
