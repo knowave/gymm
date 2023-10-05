3279354ff4a4210180ab7e4854e84fb275c04ba4
Taemin <kwaktaemin98@gmail.com>
2023-10-05 16:46:44 +0900

    추가: content column 추가

--- src/reply/entities/reply.entity.ts
+++ src/reply/entities/reply.entity.ts
@@ -3,12 +3,16 @@ import { BaseEntity } from 'src/common/entities/base.entity';
 import { Feed } from 'src/feed/entities/feed.entity';
 import { Gym } from 'src/gym/entities/gym.entity';
 import { User } from 'src/user/entities/user.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
 @InputType('replyInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class Reply extends BaseEntity {
+  @Field(() => String, { nullable: true })
+  @Column('text', { nullable: true })
+  content?: string;
+
   @Field(() => Feed, { nullable: true })
   @ManyToOne(() => Feed, (feed) => feed.replies, {
     eager: true,
