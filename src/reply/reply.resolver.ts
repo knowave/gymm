6a2b09b7c36d0c586171260e6556973a5ac47e84
Taemin <kwaktaemin98@gmail.com>
2023-10-05 17:25:53 +0900

    추가: Feed와 Gym 댓글 작성을 위한 메서드 및 리졸버 추가

--- src/reply/reply.resolver.ts
+++ src/reply/reply.resolver.ts
@@ -1,8 +1,34 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { ReplyService } from './reply.service';
 import { Reply } from './entities/reply.entity';
+import {
+  WriteReplyByFeedInput,
+  WriteReplyByFeedOutput,
+} from './dto/write-reply-by-feed.dto';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from 'src/user/entities/user.entity';
+import {
+  WriteReplyByGymInput,
+  WriteReplyByGymOutput,
+} from './dto/write-reply-by-gym.dto';
 
 @Resolver(() => Reply)
 export class ReplyResolver {
   constructor(private readonly replyService: ReplyService) {}
+
+  @Mutation(() => WriteReplyByFeedOutput)
+  async writeReplyByFeed(
+    @Args('input') writeReplyByFeedInput: WriteReplyByFeedInput,
+    @CurrentUser() user: User,
+  ): Promise<WriteReplyByFeedOutput> {
+    return this.replyService.writeReplyByFeed(writeReplyByFeedInput, user);
+  }
+
+  @Mutation(() => WriteReplyByGymOutput)
+  async wirteReplyByGym(
+    @Args('input') writeReplyByGymInput: WriteReplyByGymInput,
+    @CurrentUser() user: User,
+  ): Promise<WriteReplyByGymOutput> {
+    return this.replyService.writeReplyByGym(writeReplyByGymInput, user);
+  }
 }
--- src/reply/reply.service.ts
+++ src/reply/reply.service.ts
@@ -3,8 +3,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Feed } from 'src/feed/entities/feed.entity';
 import { Gym } from 'src/gym/entities/gym.entity';
 import { User } from 'src/user/entities/user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Reply } from './entities/reply.entity';
+import {
+  WriteReplyByFeedInput,
+  WriteReplyByFeedOutput,
+} from './dto/write-reply-by-feed.dto';
+import {
+  WriteReplyByGymInput,
+  WriteReplyByGymOutput,
+} from './dto/write-reply-by-gym.dto';
 
 @Injectable()
 export class ReplyService {
@@ -17,5 +25,76 @@ export class ReplyService {
     private readonly gymRepository: Repository<Gym>,
     @InjectRepository(Reply)
     private readonly replyRepository: Repository<Reply>,
+    private readonly dataSource: DataSource,
   ) {}
+
+  async writeReplyByFeed(
+    { feedId, content, ...writeReplyByFeedInput }: WriteReplyByFeedInput,
+    user: User,
+  ): Promise<WriteReplyByFeedOutput> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const feed = await this.feedRepository.findOne({
+        where: { id: feedId },
+      });
+
+      if (!feed) return { ok: false, error: '존재하는 Feed가 없습니다.' };
+
+      if (!content)
+        return { ok: false, error: '작성하고자 하는 댓글 내용을 적어주세요.' };
+
+      const writeReply = queryRunner.manager.create(Reply, {
+        content,
+        feed,
+        user: user,
+        ...writeReplyByFeedInput,
+      });
+
+      await queryRunner.manager.save(writeReply);
+      await queryRunner.commitTransaction();
+      return { ok: true };
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async writeReplyByGym(
+    { gymId, content, ...writeReplyByGymInput }: WriteReplyByGymInput,
+    user: User,
+  ): Promise<WriteReplyByGymOutput> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const gym = await this.gymRepository.findOne({
+        where: { id: gymId },
+      });
+
+      if (!gym) return { ok: false, error: '존재하는 Gym이 없습니다.' };
+
+      if (!content)
+        return { ok: false, error: '작성하고자 하는 댓글 내용을 적어주세요.' };
+
+      const writeReply = queryRunner.manager.create(Reply, {
+        content,
+        gym,
+        user: user,
+        ...writeReplyByGymInput,
+      });
+
+      await queryRunner.manager.save(writeReply);
+      await queryRunner.commitTransaction();
+      return { ok: true };
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
+    }
+  }
 }
