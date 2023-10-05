055cb9e9fa6669a83dac1bad6ea737318c55565f
Taemin <kwaktaemin98@gmail.com>
2023-10-05 15:55:20 +0900

    추가: TypeOrmMoudle 추가

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { LikeResolver } from './like.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Feed } from 'src/feed/entities/feed.entity';
+import { Gym } from 'src/gym/entities/gym.entity';
+import { Like } from './entities/like.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Feed, Gym, Like])],
   providers: [LikeResolver, LikeService],
 })
 export class LikeModule {}
