c668bbd5b5a6fe6a2075c7fec8ff64d0d64b5f4a
Taemin <kwaktaemin98@gmail.com>
2023-10-05 16:01:56 +0900

    추가: Feed와 Gym 좋아요 및 취소 리졸버 추가

--- src/like/like.resolver.ts
+++ src/like/like.resolver.ts
@@ -1,8 +1,34 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { LikeService } from './like.service';
 import { Like } from './entities/like.entity';
+import {
+  ToggleLikeToGymInput,
+  ToggleLikeToGymOutput,
+} from './dto/toggle-like-to-gym.dto';
+import { User } from 'src/user/entities/user.entity';
+import {
+  ToggleLikeToFeedInput,
+  ToggleLikeToFeedOutput,
+} from './dto/toggle-like-to-feed-dto';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Resolver(() => Like)
 export class LikeResolver {
   constructor(private readonly likeService: LikeService) {}
+
+  @Mutation(() => ToggleLikeToFeedOutput)
+  async toggleLikeToFeed(
+    @Args('input') toggleLikeToFeedInput: ToggleLikeToFeedInput,
+    @CurrentUser() user: User,
+  ): Promise<ToggleLikeToFeedOutput> {
+    return this.likeService.toggleLikeToFeed(toggleLikeToFeedInput, user);
+  }
+
+  @Mutation(() => ToggleLikeToGymOutput)
+  async toggleLikeToGym(
+    @Args('input') toggleLikeToGymInput: ToggleLikeToGymInput,
+    @CurrentUser() user: User,
+  ): Promise<ToggleLikeToGymOutput> {
+    return this.likeService.toggleLikeToGym(toggleLikeToGymInput, user);
+  }
 }
