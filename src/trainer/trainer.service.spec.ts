fbfa01f955faa0eb90b07a3df8310f08a34cc43d
Taemin <kwaktaemin98@gmail.com>
2023-10-06 17:40:27 +0900

    추가: Trainer 추가 unit test 완료

--- src/trainer/trainer.service.spec.ts
+++ src/trainer/trainer.service.spec.ts
@@ -4,6 +4,7 @@ import { User } from 'src/user/entities/user.entity';
 import { Test, TestingModule } from '@nestjs/testing';
 import { createConnection } from 'mysql2';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserRole } from 'src/user/enums/user-role.enum';
 
 const mockRepository = () => ({
   findOne: jest.fn(),
@@ -15,20 +16,20 @@ const mockRepository = () => ({
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
+const mockQueryRunner = {
+  manager: {},
+} as QueryRunner;
+
+class MockDataSource {
+  createQueryRunner(mode?: 'master' | 'stave'): QueryRunner {
+    return mockQueryRunner;
+  }
+}
+
 describe('TrainerService', () => {
   let trainerService: TrainerService;
   let userRepository: MockRepository<User>;
-  let dataSource: DataSource;
-
-  const mockQueryRunner = {
-    manager: {},
-  } as QueryRunner;
-
-  class MockDataSource {
-    createQueryRunner(mode?: 'master' | 'stave'): QueryRunner {
-      return mockQueryRunner;
-    }
-  }
+  let mockDataSource: DataSource;
 
   beforeEach(async () => {
     Object.assign(mockQueryRunner.manager, {
@@ -63,10 +64,89 @@ describe('TrainerService', () => {
 
     trainerService = module.get<TrainerService>(TrainerService);
     userRepository = module.get<MockRepository<User>>(getRepositoryToken(User));
-    dataSource = module.get<DataSource>(DataSource);
+    mockDataSource = module.get<DataSource>(DataSource);
   });
 
   afterEach(() => {
     jest.clearAllMocks();
+  });
+
+  describe('createTrainerByUser', () => {
+    it('기존 User가 Trainer로 변경되었을 시 Transaction Commit이 되고, Rollback은 되지 않는다.', async () => {
+      const user = new User();
+      user.id = 1;
+      user.role = UserRole.CLIENT;
+
+      userRepository.findOne.mockResolvedValue(user);
+
+      const queryRunner = mockDataSource.createQueryRunner();
+      const result = await trainerService.createTrainerByUser(
+        { role: UserRole.TRAINER },
+        user,
+      );
+
+      jest.spyOn(queryRunner.manager, 'save').mockResolvedValue(user);
+
+      expect(queryRunner.manager.save).toHaveBeenCalledTimes(1);
+      expect(userRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(queryRunner.connect).toHaveBeenCalledTimes(1);
+      expect(queryRunner.startTransaction).toHaveBeenCalledTimes(1);
+      expect(queryRunner.commitTransaction).toHaveBeenCalledTimes(1);
+      expect(queryRunner.rollbackTransaction).toHaveBeenCalledTimes(0);
+      expect(queryRunner.release).toHaveBeenCalledTimes(1);
+      expect(result).toEqual({ ok: true });
+    });
+
+    it('user가 없을 경우 Trainer 생성이 실패된다.', async () => {
+      userRepository.findOne.mockResolvedValue(null);
+
+      const result = await trainerService.createTrainerByUser(
+        { role: UserRole.TRAINER },
+        new User(),
+      );
+
+      const queryRunner = mockDataSource.createQueryRunner();
+
+      expect(queryRunner.manager.save).toHaveBeenCalledTimes(0);
+      expect(userRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(queryRunner.connect).toHaveBeenCalledTimes(1);
+      expect(queryRunner.startTransaction).toHaveBeenCalledTimes(1);
+      expect(queryRunner.commitTransaction).toHaveBeenCalledTimes(0);
+      expect(queryRunner.rollbackTransaction).toHaveBeenCalledTimes(0);
+      expect(queryRunner.release).toHaveBeenCalledTimes(1);
+
+      expect(result).toEqual({
+        ok: false,
+        error: '존재하지 않는 사용자입니다.',
+      });
+    });
+
+    it('기존 user가 role을 TRAINER가 아닌 CLIENT를 선택할 시 실패된디.', async () => {
+      const user = new User();
+      user.id = 1;
+      user.role = UserRole.CLIENT;
+
+      userRepository.findOne.mockResolvedValue(user);
+
+      const result = await trainerService.createTrainerByUser(
+        { role: UserRole.CLIENT },
+        user,
+      );
+
+      const queryRunner = mockDataSource.createQueryRunner();
+
+      expect(queryRunner.manager.save).toHaveBeenCalledTimes(0);
+      expect(userRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(queryRunner.connect).toHaveBeenCalledTimes(1);
+      expect(queryRunner.startTransaction).toHaveBeenCalledTimes(1);
+      expect(queryRunner.commitTransaction).toHaveBeenCalledTimes(0);
+      expect(queryRunner.rollbackTransaction).toHaveBeenCalledTimes(0);
+      expect(queryRunner.release).toHaveBeenCalledTimes(1);
+
+      expect(result).toEqual({
+        ok: false,
+        error: '다시 한번 역할을 확인해 주세요.',
+      });
+    });
   });
 });
