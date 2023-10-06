88809c9d8e17eba2514b2c511056ac907aba4718
Taemin <kwaktaemin98@gmail.com>
2023-10-06 10:48:44 +0900

    추가: user Follow 리졸버 추가

--- src/follow/follow.resolver.ts
+++ src/follow/follow.resolver.ts
@@ -1,7 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { FollowService } from './follow.service';
+import { FollowUserInput, FollowUserOutput } from './dto/follow-user.dto';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from 'src/user/entities/user.entity';
 
 @Resolver()
 export class FollowResolver {
   constructor(private readonly followService: FollowService) {}
+
+  @Mutation(() => FollowUserOutput)
+  async followUser(
+    @Args('input') followUserInput: FollowUserInput,
+    @CurrentUser() user: User,
+  ): Promise<FollowUserOutput> {
+    return this.followService.followUser(followUserInput, user);
+  }
 }
