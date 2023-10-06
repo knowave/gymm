be35335714c131227db6ad5b53af4193f6d4e776
Taemin <kwaktaemin98@gmail.com>
2023-10-06 14:20:22 +0900

    추가: GQL In/Output 데코레이터 누락으로 추가

--- src/gym/dto/create-gym-by-trainer.dto.ts
+++ src/gym/dto/create-gym-by-trainer.dto.ts
@@ -1,7 +1,9 @@
-import { PartialType } from "@nestjs/graphql";
-import { Gym } from "../entities/gym.entity";
-import { BaseOutput } from "src/common/dto/base-output.dto";
+import { InputType, ObjectType, PartialType } from '@nestjs/graphql';
+import { Gym } from '../entities/gym.entity';
+import { BaseOutput } from 'src/common/dto/base-output.dto';
 
+@InputType()
 export class CreateGymByTrainerInput extends PartialType(Gym) {}
 
+@ObjectType()
 export class CreateGymByTrainerOutput extends BaseOutput {}
