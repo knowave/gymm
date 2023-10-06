0f369ca0bd4f7c3675c4dce83cd227df10c1ba0d
Taemin <kwaktaemin98@gmail.com>
2023-10-06 10:21:20 +0900

    init

--- src/follow/follow.module.ts
+++ src/follow/follow.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { FollowResolver } from './follow.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  providers: [FollowResolver, FollowService]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [FollowResolver, FollowService],
 })
 export class FollowModule {}
--- src/follow/follow.resolver.ts
+++ src/follow/follow.resolver.ts
@@ -1,35 +1,7 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver } from '@nestjs/graphql';
 import { FollowService } from './follow.service';
-import { Follow } from './entities/follow.entity';
-import { CreateFollowInput } from './dto/create-follow.input';
-import { UpdateFollowInput } from './dto/update-follow.input';
 
-@Resolver(() => Follow)
+@Resolver()
 export class FollowResolver {
   constructor(private readonly followService: FollowService) {}
-
-  @Mutation(() => Follow)
-  createFollow(@Args('createFollowInput') createFollowInput: CreateFollowInput) {
-    return this.followService.create(createFollowInput);
-  }
-
-  @Query(() => [Follow], { name: 'follow' })
-  findAll() {
-    return this.followService.findAll();
-  }
-
-  @Query(() => Follow, { name: 'follow' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.followService.findOne(id);
-  }
-
-  @Mutation(() => Follow)
-  updateFollow(@Args('updateFollowInput') updateFollowInput: UpdateFollowInput) {
-    return this.followService.update(updateFollowInput.id, updateFollowInput);
-  }
-
-  @Mutation(() => Follow)
-  removeFollow(@Args('id', { type: () => Int }) id: number) {
-    return this.followService.remove(id);
-  }
 }
