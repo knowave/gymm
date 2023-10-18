dbf2bf5a9672f4e53b8f8e8df71bd31fe32747f3
Taemin <kwaktaemin98@gmail.com>
2023-10-18 15:52:19 +0900

    생성 및 추가: Auth Service 생성 및 AccessToken, RefreshToken 생성 메서드 추가

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  // AccessToken 발급
+  async createAccessToken(payload): Promise<string> {
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_ACCESS_TOKEN_SECRET'),
+      expiresIn: this.configService.get('JWT_ACCESS_TOKEN_EXPIRATION_TIME'),
+    });
+  }
+
+  // RefreshToken 발급
+  async createRefreshToken(payload): Promise<string> {
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET'),
+      expiresIn: this.configService.get('JWT_REFRESH_TOKEN_EXPIRATION_TIME'),
+    });
+  }
+}
