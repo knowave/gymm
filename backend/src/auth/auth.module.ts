e1a04083d8d3f72ef9b1db9a48520cee2a834711
Taemin <kwaktaemin98@gmail.com>
2023-10-18 15:51:41 +0900

    추가: Auth Module에 JWt 관련 Class 추가

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,31 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
+import { GqlAuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_ACCESS_TOKEN_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_ACCESS_TOKEN_SECRET')}`,
+        },
+      }),
+    }),
+  ],
+  providers: [
+    AuthService,
+    AuthResolver,
+    { provide: APP_GUARD, useClass: GqlAuthGuard },
+  ],
+  exports: [AuthService],
+})
 export class AuthModule {}
